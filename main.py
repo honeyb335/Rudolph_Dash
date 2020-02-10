d1f7c23b534e2661ccc91dbf28dda20c72962479
BuildTools <unconfigured@null.spigotmc.org>
2020-02-10 16:41:28 -0500

    bla

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ import time
 pygame.init()
 
 
-rudolph_norm = pygame.image.load('textures/norm_rudolph.png')
+norm_rudolph = pygame.image.load('textures/norm_rudolph.png')
+indigo_rudolph = pygame.image.load('textures/indigo_rudolph.png')
+turquoise_rudolph = pygame.image.load('textures/turquoise_rudolph.png')
+jade_rudolph = pygame.image.load('textures/jade_rudolph.png')
+orange_rudolph = pygame.image.load('textures/orange_rudolph.png')
+
 background = pygame.image.load('textures/bg1.jpg')
 
 
@@ -12,11 +17,21 @@ hight = 1080
 #the difference between width and hight is 840
 run = True
 FPS = 60
-vel = 5
 level = 0
+vel = 1
 
 clock = pygame.time.Clock()
 pygame.display.set_caption('Rudolph Dash')
+
+
+class rudolph0(object):
+    def __init__(self, x, y):
+        self.vel = 20
+        self.x = x       
+        self.y = y
+
+    def draw(self,window):
+        window.blit(pygame.transform.scale(norm_rudolph, (240,240)), (self.x, self.y))
 
 
 class rudolph1(object):
@@ -26,13 +41,45 @@ class rudolph1(object):
         self.y = y
 
     def draw(self,window):
-        window.blit(pygame.transform.scale(rudolph_norm, (240,240)), (self.x, self.y))
+        window.blit(pygame.transform.scale(indigo_rudolph, (240,240)), (self.x, self.y))
+
+
+class rudolph2(object):
+    def __init__(self, x, y):
+        self.vel = 20
+        self.x = x       
+        self.y = y
+
+    def draw(self,window):
+        window.blit(pygame.transform.scale(turquoise_rudolph, (240,240)), (self.x, self.y))
+
+
+class rudolph3(object):
+    def __init__(self, x, y):
+        self.vel = 20
+        self.x = x       
+        self.y = y
+
+    def draw(self,window):
+        window.blit(pygame.transform.scale(jade_rudolph, (240,240)), (self.x, self.y))
+
+
+class rudolph4(object):
+    def __init__(self, x, y):
+        self.vel = 20
+        self.x = x       
+        self.y = y
+
+    def draw(self,window):
+        window.blit(pygame.transform.scale(orange_rudolph, (240,240)), (self.x, self.y))
+
+#def level1:
 
 
 def redrawGameWindow():
     window.blit(pygame.transform.scale(background, (width,hight)), (0, 0))
-    for rudolph1 in rudolphs:
-        rudolph1.draw(window)
+    for rudolph in rudolphs:
+        rudolph.draw(window)
 
     pygame.display.update()
 
@@ -58,8 +105,10 @@ while run:
             rudolphs.pop(rudolphs.index(rudolph))
 
 
-    if len(rudolphs) < 2:
-        rudolphs.append(rudolph1(480,0))
+    if len(rudolphs) < 1:
+        rudolphs.append(rudolph4(1440,0))
+        rudolphs.append(rudolph3(960,0))
+        rudolphs.append(rudolph2(480,0))
         rudolphs.append(rudolph1(0,0))
 
  
